6bb5d456ebaa73be757886af5e12a63fec47ff2d
Thiago Valentim <thiagovalentim@hotmail.com.br>
2021-12-04 19:45:50 -0300

    Add files via upload

---
+++ FindMyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyApp.swift
+//  FindMy
+//
+//  Created by Daniel Swift on 11/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct FindMyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
