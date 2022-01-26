55108f150eb20ba8cb74c20b3a8129b4e91d13ae
Thiago Valentim <thiagovalentim@hotmail.com.br>
2022-01-25 23:03:52 -0300

    Update main.swift

--- lacosRepeticao/main.swift
+++ lacosRepeticao/main.swift
@@ -11,9 +11,9 @@ import Foundation
 // É um bloco de códicos que é possível identificar tanto de maneira manual, quanto de maneira automática.
 
 // Alguns laços de repetição:
-// while (enquanto) ->
-// repeat (repita enquanto) ->
-// for (durante) ->
+// while (enquanto)
+// repeat (repita enquanto)
+// for (durante) -> mais utilizada
 
 
 // while >> enquanto uma 'condição' não atendida, faça a verificação
@@ -25,9 +25,28 @@ var index:Int = 0
 // A condicação é: enquanto o valor de 'index'for menos ou igual a 10, continue a verificação.
 while index <= 10 {
     print("index \(index)")
-// é adicionado 1 ao valor do 'index' cada vez que ele passa na condição de ser meenor ou maior a 10.
+// é adicionado 1 ao valor do 'index' cada vez que ele passa na condição de ser menor ou maior a 10.
     index = index + 1
 }
 print("O valor do index final é: \(index)")
 
+// Repeat
+// Ela roda o loop, e no somente no final é verificado se continua ou não atendendo a condição.
+
+var indexRepeat:Int = 1
+
+repeat{
+    print("indexRepeat \(indexRepeat).")
+    indexRepeat = indexRepeat + 1
+} while indexRepeat <= 15
+    print("O valor do index final é: \(indexRepeat)")
+            
+
+// For
+// Ele permite informar onde será o inicio e o fim das repetições. Difetente do while e do repeat que o último valor era "UM" valor a mais ao ultimo número determinado.
+var value:Int = 5
+
+for value in 5...50{
+    print(value)
+}
 
